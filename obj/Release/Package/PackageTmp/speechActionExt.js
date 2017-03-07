bee3393c0cd0b57b06042f9088f408da2f874416
jin wang <v-jiwag@microsoft.com>
2017-03-07 11:19:02 +0800

    action extension add

---
+++ obj/Release/Package/PackageTmp/speechActionExt.js
@@ -0,0 +1,26 @@
+﻿(function (global) {
+    var action = ["greet", "Open TV", "Change Channel", "Close TV"];
+
+    var myPromise = function (value) {
+        return new Promise((resolve, reject) => {
+            resolve(value);
+        });
+    }
+
+    var ActionIdentiy = function () {
+        var _this = this;
+        _this.WhichAction = function (intentArr) {
+            
+            for (var item in intentArr) {
+                var out = action.find(function (value) {
+                    if (item === value) {
+                        return new myPromise(value);
+                    }
+                });
+            }
+        };
+
+        
+    }
+
+})(window)
