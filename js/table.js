41b2f05ecffe184592b9efe8ab928bf516159a9f
jin wang <v-jiwag@microsoft.com>
2017-03-24 14:14:48 +0800

    add gateway part code

--- js/table.js
+++ js/table.js
@@ -6,26 +6,58 @@
         recordEnd: "",
         uploadBegin: "",
         TextRecived: "",
-        TextWithIntention:""
+        TextWithIntention: "",
+        TotalTime : ""
     };
 
-    function addTableDetails(content) {
+
+    var contentWithDirectLatency = {
+        id: "",
+        RequestArrival: "",
+        GetAudioTextLatency: "",
+        GetAudioIntentionLatency: "",
+        ResponseArrival: "",
+        TotalTime: ""
+    };
+
+    function addTableDetails(content, typeStr) {
         var ele = document.getElementById("logTable");
         var table = document.createElement('table');
         var html = [];
         html.push("<tr>");
         html.push("<td> " + content.id + "</td>");
-        html.push("<td> " + content.recordBegin + "</td>");
-        html.push("<td> " + content.recordEnd + "</td>");
-        html.push("<td> " + content.uploadBegin + "</td>");
-        html.push("<td> " + content.TextRecived + "</td>");
-        html.push("<td> " + content.TextWithIntention + "</td>");
+        html.push("<td> " + typeStr + "</td>");
+        html.push("<td> " + "" + "</td>");
+        html.push("<td> " + (content.TextRecived - content.uploadBegin) + "</td>");
+        html.push("<td> " + (content.TextWithIntention - content.TextRecived) + "</td>");
+        html.push("<td> " + "" + "</td>");
+        html.push("<td> " + (content.TextWithIntention - content.uploadBegin) + "</td>");
         html.push("</tr>");
         table.innerHTML = html.join('');
         ele.appendChild(table.children[0].children[0]);
     }
 
+    function addTableDirectDetails(contentWithDirectLatency, typeStr) {
+        var ele = document.getElementById("logTable");
+        var table = document.createElement('table');
+        var html = [];
+        html.push("<tr>");
+        html.push("<td> " + contentWithDirectLatency.id + "</td>");
+        html.push("<td> " + typeStr + "</td>");
+        html.push("<td> " + contentWithDirectLatency.RequestArrival + "</td>");
+        html.push("<td> " + contentWithDirectLatency.GetAudioTextLatency + "</td>");
+        html.push("<td> " + contentWithDirectLatency.GetAudioIntentionLatency + "</td>");
+        html.push("<td> " + contentWithDirectLatency.ResponseArrival + "</td>");
+        html.push("<td> " + contentWithDirectLatency.TotalTime + "</td>");
+        html.push("</tr>");
+        table.innerHTML = html.join('');
+        ele.appendChild(table.children[0].children[0]);
+    }
+
+
     global.addTableDetails = addTableDetails;
+    global.addTableDirectDetails = addTableDirectDetails;
     global.content = content;
+    global.contentWithDirectLatency = contentWithDirectLatency;
 
 })(window)
--- speechActionExt.js
+++ speechActionExt.js
@@ -1,6 +1,20 @@
 ﻿(function (global) {
+
+    var content = {
+        id: "",
+        recordBegin: "",
+        recordEnd: "",
+        uploadBegin: "",
+        TextRecived: "",
+        TextWithIntention: "",
+        TotalTime : ""
+    };
+
     var serviceUrl = "https://opgwebapi.azurewebsites.net/api/Media";
+    var beginTtime = null;
+    var endTime = null;
     var sendAudio = function (view, parm, sampleRate) {
+        beginTtime = (new Date()).getTime();
         var request = new XMLHttpRequest();
         request.open('POST', [
             serviceUrl,
@@ -8,8 +22,17 @@
         ].join(""), true);
         request.setRequestHeader("Content-Type", 'audio/wav; codec="audio/pcm"; samplerate=' + sampleRate);
         request.onload = function (result) {
+            endTime = (new Date()).getTime();
             setComparisonText(result.currentTarget.responseText);
             var response = handleJSONWebResponse(request);
+            contentWithDirectLatency.id = response.Text;
+            contentWithDirectLatency.RequestArrival = (new Date(response.ArrivalTime)).getTime();
+            contentWithDirectLatency.ResponseArrival = (new Date(response.EndTime)).getTime();
+            contentWithDirectLatency.GetAudioTextLatency = response.GetAudioTextLantency;
+            contentWithDirectLatency.GetAudioIntentionLatency = response.GetAudioIntentionLantency;
+            contentWithDirectLatency.TotalTime = endTime - beginTtime;
+
+            addTableDirectDetails(contentWithDirectLatency, 'GateWay');
             console.log(response);
         }
         request.send(view);
@@ -20,6 +43,14 @@
             return JSON.parse(xhr.response);
         }
         return xhr.response;
+    }
+
+    function getUTCNow() {
+        var now = new Date();
+        var time = now.getTime();
+        var offset = now.getTimezoneOffset();
+        offset = offset * 60000;
+        return time - offset;
     }
 
     global.sendAudioToServer = sendAudio;
