d1ca0c0929cbe95ab9c29fed9ea285294cff4bf4
jin wang <v-jiwag@microsoft.com>
2017-03-29 14:15:01 +0800

    beautify json

--- speechActionExt.js
+++ speechActionExt.js
@@ -10,7 +10,7 @@
         TotalTime : ""
     };
 
-    var serviceUrl = "http://localhost:57993/api/Media";
+    var serviceUrl = "https://opgwebapi.azurewebsites.net/api/Media";
     var beginTtime = null;
     var endTime = null;
     var sendAudio = function (view, parm, sampleRate) {
@@ -23,8 +23,8 @@
         request.setRequestHeader("Content-Type", 'audio/wav; codec="audio/pcm"; samplerate=' + sampleRate);
         request.onload = function (result) {
             endTime = (new Date()).getTime();
-            setComparisonText(result.currentTarget.responseText);
             var response = handleJSONWebResponse(request);
+            setComparisonText(syntaxHighlight(response));
             contentWithDirectLatency.id = response.Text;
             contentWithDirectLatency.RequestArrival = (new Date(response.ArrivalTime)).getTime();
             contentWithDirectLatency.ResponseArrival = (new Date(response.EndTime)).getTime();
@@ -43,6 +43,29 @@
             return JSON.parse(xhr.response);
         }
         return xhr.response;
+    }
+
+    // http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+    function syntaxHighlight(json) {
+        if (typeof json != 'string') {
+            json = JSON.stringify(json, undefined, 2);
+        }
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return match;
+        });
     }
 
     function getUTCNow() {
